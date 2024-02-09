7f7a779beb6349e34d85e5beca00eaf7c295a51f
syedawardah3 <148571080+syedawardah3@users.noreply.github.com>
2024-02-09 18:44:27 +0500

    Add files via upload

---
+++ Q13/Q13.js
@@ -0,0 +1,7 @@
+// Array of favorite modes of transportation
+var transportation = ['Tesla Model S', 'Yamaha YZF-R6', 'Boeing 787 Dreamliner'];
+// Printing statements about each mode of transportation
+for (var _i = 0, transportation_1 = transportation; _i < transportation_1.length; _i++) {
+    var mode = transportation_1[_i];
+    console.log("I would like to own a ".concat(mode, "."));
+}
---
+++ Q13/Q13.ts
@@ -0,0 +1,15 @@
+// 13.Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// Array of favorite modes of transportation
+let transportation: string[] = ['Tesla Model S', 'Yamaha YZF-R6', 'Boeing 787 Dreamliner'];
+
+// Printing statements about each mode of transportation
+for (let mode of transportation) {
+    console.log(`I would like to own a ${mode}.`);
+}
+
+
+
+
+
+
